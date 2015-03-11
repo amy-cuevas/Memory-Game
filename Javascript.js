30ed3e2338c48f517d451dc6dd24eea1167f1505
amy-cuevas <acuevas@kippnyccp.org>
2015-03-11 09:13:01 -0400

    Create Javascript.js

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  var card_array = ["1", "1", "2", "2", "3", "3", "4", "4", "5", "5", "6", "6", "7", "7", "8", "8", "9", "9", "10", "10"]
+
+  for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
+  }
+});
